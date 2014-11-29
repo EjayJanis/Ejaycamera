2c69c6f4674df1cedf8fb4d6f1a10578ac41d317
EjayJanis <elainejanisdagdayan@Ejay.local>
2014-11-29 17:12:35 +0100

    initial commit

---
+++ Camera/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  Camera
+//
+//  Created by Ejay on 11/29/14.
+//  Copyright (c) 2014 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
